a8634c92b7e73b6958d36f502952a56717ae9ca5
o-t-t <18340807913@163.com>
2018-05-21 13:51:34 +0800

    购物车页面的初始化（静态页面）

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GoodsList from '@/Views/GoodsList'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/GoodsList',
       name: 'GoodsList',
       component: GoodsList
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
