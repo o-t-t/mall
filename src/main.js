a3662d01689aa53b0d7c4a28f08acbba3c89ea35
o-t-t <18340807913@163.com>
2018-05-22 12:21:04 +0800

    在main.js中使用Vue的filter全局过滤器，实现金额的格式化

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
-import VueInfiniteScroll from 'vue-infinite-scroll'
+import {currency} from './util/currency.js'   //有{}的原因，是因为currency导出的是具名的函数，所以必须要通过 object对象结构来接收
+//import VueInfiniteScroll from 'vue-infinite-scroll'
 Vue.use(VueLazyload,{
   loading: 'static/loading-svg/loading-bars.svg',
   attempt: 3
 });
-Vue.use(VueInfiniteScroll);
+Vue.filter("currency",currency);   //定义一个全局过滤器（过滤器名“currency”；值currency）
+//Vue.use(VueInfiniteScroll);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -76,7 +76,7 @@
                   </div>
                 </div>
                 <div class="cart-tab-2">
-                  <div class="item-price">{{item.salePrice}}</div>
+                  <div class="item-price">{{item.salePrice | currency('￥')}}</div>
                 </div>
                 <div class="cart-tab-3">
                   <div class="item-quantity">
@@ -90,7 +90,7 @@
                   </div>
                 </div>
                 <div class="cart-tab-4">
-                  <div class="item-price-total">{{item.productNum * item.salePrice}}</div>
+                  <div class="item-price-total">{{(item.productNum * item.salePrice) | currency('￥')}}</div>
                 </div>
                 <div class="cart-tab-5">
                   <div class="cart-item-opration">
@@ -119,7 +119,7 @@
             </div>
             <div class="cart-foot-r">
               <div class="item-total">
-                总价: <span class="total-price">{{totalPrice}}</span>
+                总价: <span class="total-price">{{totalPrice | currency('￥')}}</span>
               </div>
               <div class="btn-wrap">
                 <a class="btn btn--red">去结算</a>
